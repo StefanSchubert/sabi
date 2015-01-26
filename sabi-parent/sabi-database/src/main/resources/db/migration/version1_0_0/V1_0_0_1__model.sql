b39f337294322e1b75f3e167624b927369f590e4
StefanSchubert <StefanSchubert@users.noreply.github.com>
2015-01-26 22:21:39 +0100

    #1 db setup description and first db script

--- sabi-parent/sabi-database/src/main/resources/db/migration/version1_0_0/V1_0_0_1__model.sql
+++ sabi-parent/sabi-database/src/main/resources/db/migration/version1_0_0/V1_0_0_1__model.sql
@@ -32,9 +32,9 @@ CREATE TABLE `aquarium` (
   DEFAULT CHARSET =utf8;
 
 
-CREATE TABLE `units_lookup` (
+CREATE TABLE `unit` (
   `id`          INTEGER UNSIGNED NOT NULL,
-  `unit`        VARCHAR(5)       NOT NULL,
+  `name`        VARCHAR(15)      NOT NULL,
   `description` VARCHAR(255)     NOT NULL,
   `created_on`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -43,30 +43,120 @@ CREATE TABLE `units_lookup` (
   DEFAULT CHARSET =utf8;
 
 
-CREATE TABLE `parameter_lookup` (
-  `id`            INTEGER UNSIGNED NOT NULL,
-  `unit`          VARCHAR(5)       NOT NULL,
-  `description`   VARCHAR(255)     NOT NULL,
-  `min_threshold` FLOAT,
-  `max_threshold` FLOAT,
-  `created_on`    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+CREATE TABLE `parameter` (
+  `id`                     INTEGER UNSIGNED NOT NULL,
+  `description`            VARCHAR(255)     NOT NULL,
+  `used_threshold_unit_id` INTEGER UNSIGNED NOT NULL,
+  `min_threshold`          FLOAT,
+  `max_threshold`          FLOAT,
+  `created_on`             TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (used_threshold_unit_id) REFERENCES unit (id)
 )
   ENGINE =InnoDB
   DEFAULT CHARSET =utf8;
 
 
 CREATE TABLE `measurement` (
-  `id`            BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `measured_on`   DATETIME            NOT NULL,
-  `measued_value` FLOAT               NOT NULL,
-  `unit_id`       INTEGER   UNSIGNED  NOT NULL,
-  `parameter_id`  INTEGER    UNSIGNED NOT NULL,
-  `aquarium_id`   BIGINT(20) UNSIGNED NOT NULL,
+  `id`             BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `measured_on`    DATETIME            NOT NULL,
+  `measured_value` FLOAT               NOT NULL,
+  `unit_id`        INTEGER   UNSIGNED  NOT NULL,
+  `parameter_id`   INTEGER    UNSIGNED NOT NULL,
+  `aquarium_id`    BIGINT(20) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (unit_id) REFERENCES units_lookup (id),
-  FOREIGN KEY (parameter_id) REFERENCES parameter_lookup (id),
+  FOREIGN KEY (unit_id) REFERENCES unit (id),
+  FOREIGN KEY (parameter_id) REFERENCES parameter (id),
   FOREIGN KEY (aquarium_id) REFERENCES aquarium (id)
+)
+  ENGINE =InnoDB
+  AUTO_INCREMENT =100
+  DEFAULT CHARSET =utf8;
+
+CREATE TABLE `remedy` (
+  `id`          BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `productname` VARCHAR(60),
+  `vendor`      VARCHAR(60),
+  `created_on`  TIMESTAMP                    DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+)
+  ENGINE =InnoDB
+  AUTO_INCREMENT =100
+  DEFAULT CHARSET =utf8;
+
+
+CREATE TABLE `treatment` (
+  `id`          BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `aquarium_id` BIGINT(20) UNSIGNED NOT NULL,
+  `given_on`    DATETIME            NOT NULL,
+  `amount`      FLOAT               NOT NULL,
+  `unit_id`     INTEGER   UNSIGNED  NOT NULL,
+  `remedy_id`   BIGINT(20) UNSIGNED NOT NULL,
+  `description` VARCHAR(255),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (unit_id) REFERENCES unit (id),
+  FOREIGN KEY (aquarium_id) REFERENCES aquarium (id),
+  FOREIGN KEY (remedy_id) REFERENCES remedy (id)
+)
+  ENGINE =InnoDB
+  AUTO_INCREMENT =100
+  DEFAULT CHARSET =utf8;
+
+
+CREATE TABLE `fish_catalogue` (
+  `id`              BIGINT(20) UNSIGNED         NOT NULL AUTO_INCREMENT,
+  `scientific_name` VARCHAR(60),
+  `description`     VARCHAR(400),
+  `meerwasserwiki_url` varchar(120),
+  `created_on`      TIMESTAMP DEFAULT 0         NOT NULL,
+  `lastmod_on`      TIMESTAMP                   NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+)
+  ENGINE =InnoDB
+  AUTO_INCREMENT =100
+  DEFAULT CHARSET =utf8;
+
+CREATE TABLE `fish` (
+  `id`                BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `aquarium_id`       BIGINT(20) UNSIGNED NOT NULL,
+  `fish_catalouge_id` BIGINT(20) UNSIGNED NOT NULL,
+  `added_on`          DATETIME            NOT NULL,
+  `exodus_on`         DATETIME,
+  `nickname`          VARCHAR(60),
+  `observed_bahavior` TEXT,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (aquarium_id) REFERENCES aquarium (id),
+  FOREIGN KEY (fish_catalouge_id) REFERENCES fish_catalogue (id)
+)
+  ENGINE =InnoDB
+  AUTO_INCREMENT =100
+  DEFAULT CHARSET =utf8;
+
+
+-- todo: According classification, refactor so suite jpa inheritence model
+CREATE TABLE `coral_catalogue` (
+  `id`              BIGINT(20) UNSIGNED         NOT NULL AUTO_INCREMENT,
+  `scientific_name` VARCHAR(60),
+  `description`     VARCHAR(400),
+  `created_on`      TIMESTAMP DEFAULT 0         NOT NULL,
+  `lastmod_on`      TIMESTAMP                   NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+)
+  ENGINE =InnoDB
+  AUTO_INCREMENT =100
+  DEFAULT CHARSET =utf8;
+
+CREATE TABLE `coral` (
+  `id`                 BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `aquarium_id`        BIGINT(20) UNSIGNED NOT NULL,
+  `coral_catalouge_id` BIGINT(20) UNSIGNED NOT NULL,
+  `added_on`           DATETIME            NOT NULL,
+  `exodus_on`          DATETIME,
+  `nickname`           VARCHAR(60),
+  `observed_bahavior`  TEXT,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (aquarium_id) REFERENCES aquarium (id),
+  FOREIGN KEY (coral_catalouge_id) REFERENCES coral_catalogue (id)
 )
   ENGINE =InnoDB
   AUTO_INCREMENT =100
---
+++ sabi-parent/sabi-database/src/main/resources/db/migration/version1_0_0/V1_0_0_2__basicData.sql
@@ -0,0 +1,13 @@
+INSERT INTO unit (id, name, description) VALUES (1, 'KH', 'Karbonathärte / Alkanität');
+INSERT INTO unit (id, name, description) VALUES (2, '°C', 'Grad Celsius');
+
+INSERT INTO parameter (id, description, used_threshold_unit_id, min_threshold, max_threshold)
+VALUES (1, 'Karbonathärte', 1, 6.5, 10);
+
+INSERT INTO parameter (id, description, used_threshold_unit_id, min_threshold, max_threshold)
+VALUES (2, 'Temperatur', 2, 24, 27);
+
+INSERT INTO remedy (id, productname, vendor) VALUES (1, 'KH+', 'Dupla');
+
+INSERT INTO fish_catalogue (id, scientific_name, description, meerwasserwiki_url)
+VALUES (1, 'Acreichthys tomentosus', 'Seegras feilenfisch', 'http://meerwasserwiki.de/w/index.php?title=Acreichthys_tomentosus');
