f0a0f0f85e388d943303ca2987e65e9382113e0b
Stefan Schubert <stefan.schubert@accso.de>
2015-01-26 22:08:50 +0100

    #1 db setup description and first db script

--- sabi-parent/sabi-database/src/main/resources/db/migration/version1_0_0/V1_0_0_1__model.sql
+++ sabi-parent/sabi-database/src/main/resources/db/migration/version1_0_0/V1_0_0_1__model.sql
@@ -1,15 +1,73 @@
 CREATE TABLE `users` (
-  `id`             BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `EMAIL`          VARCHAR(255)        NOT NULL,
-  `PASSWORD`       VARCHAR(255)        NOT NULL,
-  `CREATED_ON`     TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `VALIDATE_TOKEN` VARCHAR(255)        NOT NULL,
-  `VALIDATED`      BOOLEAN                      DEFAULT FALSE,
+  `id`             BIGINT(20) UNSIGNED   NOT NULL         AUTO_INCREMENT,
+  `email`          VARCHAR(255)          NOT NULL,
+  `password`       VARCHAR(255)          NOT NULL,
+  `validate_token` VARCHAR(255)          NOT NULL,
+  `validated`      BOOLEAN               NOT NULL         DEFAULT FALSE,
+  `created_on`     TIMESTAMP DEFAULT 0   NOT NULL,
+  `lastmod_on`     TIMESTAMP             NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UNQ_EMAIL` (`EMAIL`)
+  UNIQUE KEY `UNQ_EMAIL` (`email`)
 )
   ENGINE =InnoDB
   AUTO_INCREMENT =100
-  DEFAULT CHARSET =utf8
+  DEFAULT CHARSET =utf8;
 
 
+CREATE TABLE `aquarium` (
+  `id`             BIGINT(20) UNSIGNED         NOT NULL AUTO_INCREMENT,
+  `size`           INTEGER,
+  `size_unit`      VARCHAR(10),
+  `validate_token` VARCHAR(255)                NOT NULL,
+  `active`         BOOLEAN                              DEFAULT FALSE,
+  `user_id`        BIGINT(20) UNSIGNED,
+  `created_on`     TIMESTAMP DEFAULT 0         NOT NULL,
+  `lastmod_on`     TIMESTAMP                   NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+    ON DELETE SET NULL
+)
+  ENGINE =InnoDB
+  AUTO_INCREMENT =100
+  DEFAULT CHARSET =utf8;
+
+
+CREATE TABLE `units_lookup` (
+  `id`          INTEGER UNSIGNED NOT NULL,
+  `unit`        VARCHAR(5)       NOT NULL,
+  `description` VARCHAR(255)     NOT NULL,
+  `created_on`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =utf8;
+
+
+CREATE TABLE `parameter_lookup` (
+  `id`            INTEGER UNSIGNED NOT NULL,
+  `unit`          VARCHAR(5)       NOT NULL,
+  `description`   VARCHAR(255)     NOT NULL,
+  `min_threshold` FLOAT,
+  `max_threshold` FLOAT,
+  `created_on`    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =utf8;
+
+
+CREATE TABLE `measurement` (
+  `id`            BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `measured_on`   DATETIME            NOT NULL,
+  `measued_value` FLOAT               NOT NULL,
+  `unit_id`       INTEGER   UNSIGNED  NOT NULL,
+  `parameter_id`  INTEGER    UNSIGNED NOT NULL,
+  `aquarium_id`   BIGINT(20) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (unit_id) REFERENCES units_lookup (id),
+  FOREIGN KEY (parameter_id) REFERENCES parameter_lookup (id),
+  FOREIGN KEY (aquarium_id) REFERENCES aquarium (id)
+)
+  ENGINE =InnoDB
+  AUTO_INCREMENT =100
+  DEFAULT CHARSET =utf8;
