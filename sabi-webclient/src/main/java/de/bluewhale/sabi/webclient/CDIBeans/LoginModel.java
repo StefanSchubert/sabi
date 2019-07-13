6c4addd24ccc41bd34f3646e8ad0a849a135de98
Stefan Schubert <stefan@bluewhale.de>
2019-07-13 14:59:46 +0200

    #sabi-12 WebGUI for User login/register - initial jsf/primefaces layout

---
+++ sabi-webclient/src/main/java/de/bluewhale/sabi/webclient/CDIBeans/LoginModel.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 by Stefan Schubert
+ */
+
+package de.bluewhale.sabi.webclient.CDIBeans;
+
+import javax.inject.Named;
+
+/**
+ * Model for the Login Form.
+ *
+ *
+ * @author Stefan Schubert
+ */
+@Named
+public class LoginModel {
+
+    private String firstName = "John";
+    private String lastName = "Doe";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String showGreeting() {
+        return "Hello " + firstName + " " + lastName + "!";
+    }
+}
---
+++ sabi-webclient/src/main/java/de/bluewhale/sabi/webclient/SpringPrimeFacesApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2019 by Stefan Schubert
+ */
+
+package de.bluewhale.sabi.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrimeFacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPrimeFacesApplication.class, args);
+    }
+}
