f46293511589b517ce6c2f3df3a7226237de3c05
Stefan Schubert <stefan@bluewhale.de>
2021-12-15 22:09:45 +0100

    Some lib upgrades (log4j)

--- devops/sabi_docker_sdk/copyjars.sh
+++ devops/sabi_docker_sdk/copyjars.sh
@@ -3,5 +3,5 @@ echo "copy build jars to container assets."
 mkdir -p captcha/assets/opt
 mkdir -p sabi-backend/assets/opt
 cp ../../captcha/target/captcha-service.jar captcha/assets/opt
-cp ../../sabi-server/target/sabi-service-1.0-SNAPSHOT.jar sabi-backend/assets/opt
+cp ../../sabi-server/target/sabi-service.jar sabi-backend/assets/opt
 echo "done"
