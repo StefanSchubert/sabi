47e64aa344bc042f8859e800b84f4b59547e58cf
Stefan Schubert <stefan@bluewhale.de>
2022-07-15 21:36:55 +0200

    Upgraded flyway to 9.x / added db table to store plague records.

--- sabi-database/src/main/resources/db/migration/version1_0_0/V1_0_0_16__addPlagueModel.sql
+++ sabi-database/src/main/resources/db/migration/version1_0_0/V1_0_0_16__addPlagueModel.sql
@@ -32,10 +32,10 @@ CREATE TABLE `localized_plague`
 
 CREATE TABLE `plague_status`
 (
-    `id`              BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `created_on`      TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `lastmod_on`      TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `optlock`         INT UNSIGNED        NOT NULL DEFAULT 0,
+    `id`         BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_on` TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `lastmod_on` TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `optlock`    INT UNSIGNED        NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`)
 )
     ENGINE = InnoDB
@@ -44,13 +44,13 @@ CREATE TABLE `plague_status`
 
 CREATE TABLE `localized_plague_status`
 (
-    `id`          BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `plague_status_id`   BIGINT(20) UNSIGNED NOT NULL,
-    `description` VARCHAR(80)         NOT NULL,
-    `language`    VARCHAR(3)          NOT NULL,
-    `created_on`  TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `lastmod_on`  TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `optlock`     INT UNSIGNED        NOT NULL DEFAULT 0,
+    `id`               BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `plague_status_id` BIGINT(20) UNSIGNED NOT NULL,
+    `description`      VARCHAR(80)         NOT NULL,
+    `language`         VARCHAR(3)          NOT NULL,
+    `created_on`       TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `lastmod_on`       TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `optlock`          INT UNSIGNED        NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`),
     UNIQUE KEY `business_key` (`plague_status_id`, `language`),
     foreign key (plague_status_id) references plague_status (id)
@@ -59,7 +59,23 @@ CREATE TABLE `localized_plague_status`
     AUTO_INCREMENT = 1
     DEFAULT CHARSET = utf8;
 
-
+CREATE TABLE `plague_record`
+(
+    `id`                     BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `aquarium_id`            BIGINT(20) UNSIGNED NOT NULL,
+    `observed_on`            TIMESTAMP           NOT NULL,
+    `plague_id`              BIGINT(20) UNSIGNED NOT NULL,
+    `observed_plague_status` BIGINT(20) UNSIGNED NOT NULL,
+    `created_on`             TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `lastmod_on`             TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (plague_id) REFERENCES plague (id),
+    FOREIGN KEY (observed_plague_status) REFERENCES plague_status (id),
+    FOREIGN KEY (aquarium_id) REFERENCES aquarium (id)
+)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 100
+    DEFAULT CHARSET = utf8;
 
 -- Basic Data:
 INSERT INTO plague (id, scientific_name, created_on, lastmod_on)
@@ -91,15 +107,15 @@ VALUES (5, '2022-06-05 22:00:00', '2022-06-05 22:00:00');
 
 -- english
 INSERT INTO localized_plague_status (id, plague_status_id, description, language, created_on, lastmod_on)
-VALUES (1,1,'First spots visible','en','2022-06-05 22:00:00', '2022-06-05 22:00:00');
+VALUES (1, 1, 'First spots visible', 'en', '2022-06-05 22:00:00', '2022-06-05 22:00:00');
 INSERT INTO localized_plague_status (id, plague_status_id, description, language, created_on, lastmod_on)
-VALUES (2,2,'Seems to be spreading','en','2022-06-05 22:00:00', '2022-06-05 22:00:00');
+VALUES (2, 2, 'Seems to be spreading', 'en', '2022-06-05 22:00:00', '2022-06-05 22:00:00');
 INSERT INTO localized_plague_status (id, plague_status_id, description, language, created_on, lastmod_on)
-VALUES (3,3,'Growing seems to stop','en','2022-06-05 22:00:00', '2022-06-05 22:00:00');
+VALUES (3, 3, 'Growing seems to stop', 'en', '2022-06-05 22:00:00', '2022-06-05 22:00:00');
 INSERT INTO localized_plague_status (id, plague_status_id, description, language, created_on, lastmod_on)
-VALUES (4,4,'Slowly retreating','en','2022-06-05 22:00:00', '2022-06-05 22:00:00');
+VALUES (4, 4, 'Slowly retreating', 'en', '2022-06-05 22:00:00', '2022-06-05 22:00:00');
 INSERT INTO localized_plague_status (id, plague_status_id, description, language, created_on, lastmod_on)
-VALUES (5,5,'Vanished (will close plage log)','en','2022-06-05 22:00:00', '2022-06-05 22:00:00');
+VALUES (5, 5, 'Vanished (will close plage log)', 'en', '2022-06-05 22:00:00', '2022-06-05 22:00:00');
 
 /*
  * Copyright (c) 2022 by Stefan Schubert under the MIT License (MIT).
@@ -108,12 +124,18 @@ VALUES (5,5,'Vanished (will close plage log)','en','2022-06-05 22:00:00', '2022-
 
 -- german
 INSERT INTO localized_plague_status (id, plague_status_id, description, language, created_on, lastmod_on)
-VALUES (6,1,'Erstes Aufkommen','de','2022-06-05 22:00:00', '2022-06-05 22:00:00');
+VALUES (6, 1, 'Erstes Aufkommen', 'de', '2022-06-05 22:00:00', '2022-06-05 22:00:00');
 INSERT INTO localized_plague_status (id, plague_status_id, description, language, created_on, lastmod_on)
-VALUES (7,2,'Verbreitet sich','de','2022-06-05 22:00:00', '2022-06-05 22:00:00');
+VALUES (7, 2, 'Verbreitet sich', 'de', '2022-06-05 22:00:00', '2022-06-05 22:00:00');
 INSERT INTO localized_plague_status (id, plague_status_id, description, language, created_on, lastmod_on)
-VALUES (8,3,'Ausdehnung stagniert','de','2022-06-05 22:00:00', '2022-06-05 22:00:00');
+VALUES (8, 3, 'Ausdehnung stagniert', 'de', '2022-06-05 22:00:00', '2022-06-05 22:00:00');
 INSERT INTO localized_plague_status (id, plague_status_id, description, language, created_on, lastmod_on)
-VALUES (9,4,'Langsam rückgänig','de','2022-06-05 22:00:00', '2022-06-05 22:00:00');
+VALUES (9, 4, 'Langsam rückgänig', 'de', '2022-06-05 22:00:00', '2022-06-05 22:00:00');
 INSERT INTO localized_plague_status (id, plague_status_id, description, language, created_on, lastmod_on)
-VALUES (10,5,'Verschwunden (plague beendet, log schließt)','de','2022-06-05 22:00:00', '2022-06-05 22:00:00');
+VALUES (10, 5, 'Verschwunden (plague beendet, log schließt)', 'de', '2022-06-05 22:00:00', '2022-06-05 22:00:00');
+
+-- Some Testdata
+INSERT INTO plague_record (id, aquarium_id, observed_on, plague_id, observed_plague_status, created_on, lastmod_on)
+VALUES (1, 1, '2022-06-05 22:00:00', 1, 1, '2022-06-05 22:00:00', '2022-06-05 22:00:00');
+INSERT INTO plague_record (id, aquarium_id, observed_on, plague_id, observed_plague_status, created_on, lastmod_on)
+VALUES (2, 1, '2022-07-05 08:00:00', 1, 5, '2022-07-05 08:00:00', '2022-07-08 22:00:00');
