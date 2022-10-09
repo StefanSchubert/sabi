ae426e263066a3952f78a20dad54194b198d4cd2
Stefan Schubert <stefan@bluewhale.de>
2022-10-09 17:48:53 +0200

    #fixes sabi-117

---
+++ sabi-database/src/main/resources/db/migration/version1_0_1/V1_0_1_1__addMorePlagueTypes.sql
@@ -0,0 +1,15 @@
+
+/*
+ * Copyright (c) 2022 by Stefan Schubert under the MIT License (MIT).
+ * See project LICENSE file for the detailed terms and conditions.
+ */
+
+-- Basic Data:
+INSERT INTO plague (id, scientific_name, created_on, lastmod_on)
+VALUES (5, 'Zygnema stellinum', '2022-06-05 22:00:00', '2022-06-05 22:00:00');
+
+INSERT INTO localized_plague (id, plague_id, common_name, language, created_on, lastmod_on)
+VALUES (9, 5, 'Filamentous algae', 'en', '2022-09-05 22:00:00', '2022-09-05 22:00:00');
+INSERT INTO localized_plague (id, plague_id, common_name, language, created_on, lastmod_on)
+VALUES (10, 5, 'Fadenalge/Sternalge', 'de', '2022-09-05 22:00:00', '2022-09-05 22:00:00');
+
