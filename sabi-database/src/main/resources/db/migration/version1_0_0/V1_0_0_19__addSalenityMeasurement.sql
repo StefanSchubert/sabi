ea99c90b8fa0ad50e61540b0b68bfa5cc4555493
Stefan Schubert <stefan@bluewhale.de>
2022-10-09 10:38:30 +0200

    Update V1_0_0_19__addSalenityMeasurement.sql

--- sabi-database/src/main/resources/db/migration/version1_0_0/V1_0_0_19__addSalenityMeasurement.sql
+++ sabi-database/src/main/resources/db/migration/version1_0_0/V1_0_0_19__addSalenityMeasurement.sql
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2022 by Stefan Schubert under the MIT License (MIT).
- * See project LICENSE file for the detailed terms and conditions.
- */
-
 INSERT INTO unit (id, name, description) VALUES (11, 'Salinity (g/ml)', 'Salinity');
 
 INSERT INTO parameter (id, description, belonging_unit_id, min_threshold, max_threshold)
