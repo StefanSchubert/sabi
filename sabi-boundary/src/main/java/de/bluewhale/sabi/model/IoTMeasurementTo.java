c6c4fc1ee524d6745d42812d27cf4140c45c45ec
Stefan Schubert <stefan@bluewhale.de>
2022-12-17 14:53:37 +0100

    Upgraded deps and springdoc

--- sabi-boundary/src/main/java/de/bluewhale/sabi/model/IoTMeasurementTo.java
+++ sabi-boundary/src/main/java/de/bluewhale/sabi/model/IoTMeasurementTo.java
@@ -6,11 +6,11 @@
 package de.bluewhale.sabi.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
