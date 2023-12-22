d74397365759031b82e2ed1a855a69d93e65f9fe
Stefan Schubert <stefan@bluewhale.de>
2023-12-22 22:13:49 +0100

    sabi-146 Open Project to Freshwater-Users - OWASP Patch Management. Introduced API-Key Usage for OWASP.

---
+++ sabi-boundary/src/main/java/de/bluewhale/sabi/model/WaterType.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023 by Stefan Schubert under the MIT License (MIT).
+ * See project LICENSE file for the detailed terms and conditions.
+ */
+
+package de.bluewhale.sabi.model;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * Used to distinguish the tank types and therefore the measurement ecologic reference system.
+ *
+ * @author Stefan Schubert
+ */
+public enum WaterType {
+
+    SEA_WATER("seawater"),
+    FRESH_WATER("freshwater");
+
+
+    WaterType(final String waterType) {
+        this.waterType = waterType;
+    }
+    @JsonValue
+    public String getWaterType() {
+        return this.waterType;
+    }
+
+    private String waterType;
+
+    @Override
+    public String toString() {
+        return "WaterType{" +
+                "waterType='" + waterType + '\'' +
+                '}';
+    }
+}
