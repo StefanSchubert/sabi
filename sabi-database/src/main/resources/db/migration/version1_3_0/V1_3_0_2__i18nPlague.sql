6f1c7ac8353a101071073f24e93fff2cfc95d152
Stefan Schubert <stefan@bluewhale.de>
2024-02-26 21:49:41 +0100

    sabi-17 i18n of measurement, unit, parameters name and description - plagues - parameters

---
+++ sabi-database/src/main/resources/db/migration/version1_3_0/V1_3_0_2__i18nPlague.sql
@@ -0,0 +1,99 @@
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (6, 'Stella marina Asterina', 'it', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (5, 'Alghe filamentose', 'it', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (4, 'Dinoflagellate', 'it', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (3, 'Vermi piatti', 'it', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (2, 'Alghe a bolla', 'it', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (1, 'Cianobatteri', 'it', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (6, 'Étoile de mer Asterina', 'fr', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (5, 'Algues filamenteuses', 'fr', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (4, 'Dinoflagellé', 'fr', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (3, 'Vers plats', 'fr', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (2, 'Algues-bulles', 'fr', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (1, 'Cyanobactéries', 'fr', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (6, 'Estrella de mar Asterina', 'es', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (5, 'Algas filamentosas', 'es', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (4, 'Dinoflagelados', 'es', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (3, 'Gusanos planos', 'es', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (2, 'Algas burbuja', 'es', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague (plague_id, common_name, language, created_on, lastmod_on, optlock)
+VALUES (1, 'Cianobacterias', 'es', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (1, 'Primera aparición', 'es', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (2, 'Se extiende', 'es', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (3, 'La expansión se estanca', 'es', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (4, 'Lento declive', 'es', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (5, 'Desaparición (fin de la plaga)', 'es', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (1, 'Première apparition', 'fr', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (2, 'Se propage', 'fr', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (3, 'Extension stagnante', 'fr', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (4, 'Recule lentement', 'fr', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (5, 'Disparaît (fin du fléau)', 'fr', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (1, 'Prima comparsa', 'it', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (2, 'Si diffonde', 'it', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (3, 'L\'espansione ristagna', 'it', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (4, 'Lentamente diminuisce', 'it', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO localized_plague_status (plague_status_id, description, language, created_on, lastmod_on, optlock)
+VALUES (5, 'Scomparsa (fine della peste)', 'it', DEFAULT, DEFAULT, DEFAULT);
+
