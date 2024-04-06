ea495b97cdee2112b0988ec58e6c4a3aea1b3ba5
Stefan Schubert <stefan@bluewhale.de>
2024-04-06 22:41:29 +0200

    sabi-157 Test case classification and use of test containers for integration tests - Refactored test location of controllers - Migrated CoralServiceTest - Introduced first suite classes, for selective test execution.

---
+++ sabi-server/src/test/java/de/bluewhale/sabi/IntegrationTestSuite.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024 by Stefan Schubert under the MIT License (MIT).
+ * See project LICENSE file for the detailed terms and conditions.
+ */
+
+package de.bluewhale.sabi;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"de.bluewhale.sabi.persistence","de.bluewhale.sabi.rest.controller"})
+@IncludeTags("IntegrationTest")
+public class IntegrationTestSuite {
+	// Diese Klasse bleibt leer. Sie dient nur als Halter für die obigen Annotationen
+}
